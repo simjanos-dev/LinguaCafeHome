70f119e3a4677be7802f7ac08fe3894a7164af61
simjanos-dev <simjanos.dev@gmail.com>
2024-03-18 14:27:35 +0100

    Revert "Added user manual"
    This reverts commit 76ddd2c638892df2008a43163509a4de9ba5d007.

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+	<v-app>
+		<v-app-bar
+			app
+			color="primary"
+			light
+			dense
+			flat
+		>
+			<v-btn rounded text plain>Overview</v-btn>
+		</v-app-bar>
+
+		<v-main>
+			<Home/>
+		</v-main>
+	</v-app>
+</template>
+
+<script>
+import Home from './components/Home';
+
+export default {
+  name: 'App',
+
+  components: {
+    Home,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,79 +1,10 @@
-// import {Vue, createApp} from 'vue'
-// import VueRouter from 'vue-router';
-// import vuetify from './plugins/vuetify'
-
-// Vue.config.productionTip = false
-
-// window.Vue = require('vue').default;
-// window.Vue.use(VueRouter);
-
-// import Layout from './components/Layout.vue';
-// import Layout from './components/Home.vue';
-
-// const router = new VueRouter({
-//   mode: 'history',
-//   routes: [
-//       { path: '/', component: Home },
-
-//   ]
-// })
-
-// const app = new Vue({
-//   router,
-//   el: '#app',
-//   vuetify
-// });
-
-// const app = createApp(Layout)
-// app.use(router);
-// app.mount('#app')
-//////////////////////////////////////6
-
 import Vue from 'vue'
-import VueRouter from 'vue-router';
+import App from './App.vue'
 import vuetify from './plugins/vuetify'
 
-window.Vue = require('vue').default;
-window.Vue.use(VueRouter);
+Vue.config.productionTip = false
 
-import Layout from './components/Layout.vue';
-Vue.component('layout', Layout);
-
-// user manual
-const UserManual = require('./components/UserManual/UserManual.vue').default;
-import UserManualIntroduction from './components/UserManual/Pages/UserManualIntroduction';
-import UserManualBackup from './components/UserManual/Pages/UserManualBackup';
-import UserManualLanguages from './components/UserManual/Pages/UserManualLanguages';
-import UserManualReading from './components/UserManual/Pages/UserManualReading';
-import UserManualVocabularyImport from './components/UserManual/Pages/UserManualVocabularyImport';
-
-Vue.component('user-manual-introduction', UserManualIntroduction);
-Vue.component('user-manual-backup', UserManualBackup);
-Vue.component('user-manual-languages', UserManualLanguages);
-Vue.component('user-manual-reading', UserManualReading);
-Vue.component('user-manual-vocabulary-import', UserManualVocabularyImport);
-
-const Home = require('./components/Home.vue').default;
-
-const router = new VueRouter({
-    mode: 'history',
-    routes: [
-        { path: '/', component: Home },
-        { path: '/user-manual/:currentPage?', component: UserManual },
-    ]
-})
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    router,
-    vuetify,
-    el: '#app',
-    render: function (h) { return h(Layout) }
-});
-
-
+new Vue({
+  vuetify,
+  render: function (h) { return h(App) }
+}).$mount('#app')
